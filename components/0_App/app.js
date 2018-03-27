7be725307d58abb02a98f3deccfad472a4f66fe0
Cho Lin Tsai <max19851113@gmail.com>
2018-03-27 21:35:47 +0800

    found a cool package react-youtube

--- components/0_App/app.js
+++ components/0_App/app.js
@@ -5,14 +5,15 @@ import './app.scss';
 
 import Header from '../1_Header/header';
 import Navbar from '../2_Navbar/navbar';
-import SecDaredevil from '../3_Sec_Daredevil/sec_daredevil';
+import Daredevil from '../3_Daredevil/daredevil';
 
 class App extends React.Component {
 
   render() {
-    return (<div id="app">
+    return (
+    <div id="app">
       <Header />
-      <SecDaredevil />
+      <Daredevil />
     </div>)
   }
 }
