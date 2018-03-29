5b747270dd9a09f8940fffa9ccd50bb3bd8d0d00
Cho Lin Tsai <max19851113@gmail.com>
2018-03-29 21:29:15 +0800

    daredevil section is wip

---
+++ components/3_Banner_Daredevil/Banner_daredevil.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import style from './Banner_daredevil.scss'
+
+class BannerDaredevil extends Component {
+  render() {
+    return (<section className={style.bannerDaredevil}>
+      <p>Daredevil</p>
+      <figure></figure>
+    </section>)
+  }
+}
+
+export default BannerDaredevil;
