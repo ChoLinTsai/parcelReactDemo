0b0919c4f4aa7a3e79cbcbf0d1ddb6bb98354234
Cho Lin Tsai <max19851113@gmail.com>
2018-03-30 21:33:52 +0800

    not much progress, but learned some new stuff

--- components/4_Daredevil/daredevil.js
+++ components/4_Daredevil/daredevil.js
@@ -1,11 +1,23 @@
-import React, {Component} from 'react';
+import React, {Component} from "react";
+import ReactDOM from "react-dom";
 import style from './daredevil.scss';
 import Youtube from "react-youtube"
 
 class Daredevil extends Component {
 
+  componentWillMount() {
+    window.addEventListener('scroll', () => this.onScroll(), true);
+  }
+
+  onScroll() {
+    let videoNode = ReactDOM.findDOMNode(this.refs.video);
+    let windowY = window.scrollY;
+    console.log(videoNode.offsetTop);
+    console.log(windowY);
+  }
+
   videoOn() {
-    console.log("this.state.onPlay");
+    console.log("Video on Play");
   }
 
   videoPause() {
@@ -22,14 +34,15 @@ class Daredevil extends Component {
     }
 
     return (
-			<section className={style.daredevilContent}>
-				<Youtube
-				videoId="2Cn3DVV0LHY"
-				opts={opts}
-				onPlay={() => this.videoOn()}
-				onPause={this.videoPause} />
-			</section>
-		);
+      <section className={style.daredevilContent}>
+        <Youtube
+          videoId="2Cn3DVV0LHY"
+          opts={opts}
+          onPlay={() => this.videoOn()}
+          onPause={this.videoPause}
+          ref="video" />
+      </section>
+    );
   }
 }
 
